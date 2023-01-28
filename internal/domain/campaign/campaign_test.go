46d8ac4d6a6c5914d1111e3e1183a1dce8bb47db
gustavodiniz <gustavodinniz@hotmail.com>
2023-01-27 22:13:05 -0300

    test: added tests createCampaign, IdIsNotNill and CreatedOnIsNotNill

--- internal/domain/campaign/campaign_test.go
+++ internal/domain/campaign/campaign_test.go
@@ -2,11 +2,12 @@ package campaign
 
 import (
 	"testing"
+	"time"
 
 	"github.com/stretchr/testify/assert"
 )
 
-func TestNewCampaign(t *testing.T) {
+func Test_NewCampaign_CreateCampaign(t *testing.T) {
 	assert := assert.New(t)
 	name := "Campaign X"
 	content := "Body"
@@ -14,8 +15,30 @@ func TestNewCampaign(t *testing.T) {
 
 	campaign := NewCampaign(name, content, contacts)
 
-	assert.Equal(campaign.ID, "1")
 	assert.Equal(campaign.Name, "Campaign X")
 	assert.Equal(campaign.Content, "Body")
 	assert.Equal(len(campaign.Contacts), len(contacts))
+}
+
+func Test_NewCampaign_IDIsNotNill(t *testing.T) {
+	assert := assert.New(t)
+	name := "Campaign X"
+	content := "Body"
+	contacts := []string{"email1@e.com", "email2@e.com"}
+
+	campaign := NewCampaign(name, content, contacts)
+
+	assert.NotNil(campaign.ID)
+}
+
+func Test_NewCampaign_CreatedOnIsNotNill(t *testing.T) {
+	assert := assert.New(t)
+	name := "Campaign X"
+	content := "Body"
+	contacts := []string{"email1@e.com", "email2@e.com"}
+	now := time.Now().Add(-time.Minute)
+
+	campaign := NewCampaign(name, content, contacts)
+
+	assert.Greater(campaign.CreatedOn, now)
 }
