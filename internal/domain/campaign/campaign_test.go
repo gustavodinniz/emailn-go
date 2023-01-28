7a701fa47dd4031f9748f30883ee9d763c21dacf
gustavodiniz <gustavodinniz@hotmail.com>
2023-01-28 11:24:06 -0300

    test: refactor tests for campaing

--- internal/domain/campaign/campaign_test.go
+++ internal/domain/campaign/campaign_test.go
@@ -7,11 +7,14 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
+var (
+	name     = "Campaign X"
+	content  = "Body"
+	contacts = []string{"email1@e.com", "email2@e.com"}
+)
+
 func Test_NewCampaign_CreateCampaign(t *testing.T) {
 	assert := assert.New(t)
-	name := "Campaign X"
-	content := "Body"
-	contacts := []string{"email1@e.com", "email2@e.com"}
 
 	campaign := NewCampaign(name, content, contacts)
 
@@ -22,20 +25,14 @@ func Test_NewCampaign_CreateCampaign(t *testing.T) {
 
 func Test_NewCampaign_IDIsNotNill(t *testing.T) {
 	assert := assert.New(t)
-	name := "Campaign X"
-	content := "Body"
-	contacts := []string{"email1@e.com", "email2@e.com"}
 
 	campaign := NewCampaign(name, content, contacts)
 
 	assert.NotNil(campaign.ID)
 }
 
-func Test_NewCampaign_CreatedOnIsNotNill(t *testing.T) {
+func Test_NewCampaign_CreatedOnMustBeNow(t *testing.T) {
 	assert := assert.New(t)
-	name := "Campaign X"
-	content := "Body"
-	contacts := []string{"email1@e.com", "email2@e.com"}
 	now := time.Now().Add(-time.Minute)
 
 	campaign := NewCampaign(name, content, contacts)
